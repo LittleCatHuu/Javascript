bded73691a408a64270cb965765244f2b5f62058
LittleCatHuu <zx123126zx@163.com>
2017-02-27 22:40:13 +0800

    second,javascript

--- src/main/java/com/example/javascript/MainActivity.java
+++ src/main/java/com/example/javascript/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.javascript;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("web.html");
+        mWebView.addJavascriptInterface(MainActivity.this,"android");
+
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mWebView.loadUrl("javascript:javacalljs()");
+            }
+        });
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mWebView.loadUrl("javascript:javacalljswith("+"'www.baidu.com'"+")");
+            }
+        });
+    }
+    @JavascriptInterface
+    public void startFunction(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this,"show",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    @JavascriptInterface
+    public void startFunction(final String text){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new AlertDialog.Builder(MainActivity.this).setMessage(text).show();
+
+            }
+        });
     }
 }
